6706e9311152b5b9158215ad4afd07b1e345065c
ootidea <ootidea@gmail.com>
2022-06-05 15:34:33 +0900

    feat: add Divider component

---
+++ src/lib/Divider.tsx
@@ -0,0 +1,32 @@
+import { mergeProps, splitProps } from 'solid-js'
+import { ParentProps } from 'solid-js/types/render/component'
+import './Divider.scss'
+import { BaseProps, joinClasses, joinStyles } from './utility/props'
+
+export function Divider(
+  rawProps: ParentProps<BaseProps & { direction?: 'horizontal' | 'vertical'; thickness?: string; color?: string }>
+) {
+  const [props, restProps] = splitProps(
+    mergeProps(
+      {
+        direction: 'horizontal',
+        thickness: 'var(--skel-Divider_default-thickness)',
+        color: 'var(--skel-Divider_default-color)',
+      },
+      rawProps
+    ),
+    ['class', 'classList', 'style', 'direction', 'thickness', 'color']
+  )
+
+  return (
+    <div
+      class={`skel-Divider_root ${joinClasses(props.class, props.classList)}`}
+      style={joinStyles(props.style, {
+        '--skel-Divider_thickness': props.thickness,
+        '--skel-Divider_color': props.color,
+      })}
+      data-direction={props.direction}
+      {...restProps}
+    />
+  )
+}
