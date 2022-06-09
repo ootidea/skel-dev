6cbc72481099988bc6ca2ec8696dae74d8958f44
ootidea <ootidea@gmail.com>
2022-06-10 08:51:43 +0900

    fix: delete accidentally committed codes

--- src/page/CheckboxComponent.tsx
+++ src/page/CheckboxComponent.tsx
@@ -1,20 +1,13 @@
-import { createSignal } from 'solid-js'
 import { Checkbox } from '../lib/Checkbox'
 import { PageTitle } from '../PageTitle'
 import { Sample } from '../Sample'
 
 export function CheckboxComponent() {
-  const [get, set] = createSignal(true)
   return (
     <article>
       <PageTitle>Checkbox</PageTitle>
       <Sample>
-        <Checkbox checked={get()} onChange={set}>
-          Agree
-        </Checkbox>
-        <Checkbox checked={get()} onChange={set}>
-          Agree
-        </Checkbox>
+        <Checkbox>Agree</Checkbox>
       </Sample>
     </article>
   )
