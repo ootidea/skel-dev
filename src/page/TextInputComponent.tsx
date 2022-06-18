e18a77672c5a0aa42f097729b46beacba0136124
ootidea <ootidea@gmail.com>
2022-06-18 13:51:56 +0900

    feat: add TextInput samples

--- src/page/TextInputComponent.tsx
+++ src/page/TextInputComponent.tsx
@@ -1,4 +1,5 @@
 import { Icon } from '../lib/Icon'
+import { Spinner } from '../lib/Spinner'
 import { TextInput } from '../lib/TextInput'
 import { PageTitle } from '../PageTitle'
 import { Sample } from '../Sample'
@@ -42,11 +43,13 @@ export function TextInputComponent() {
       <Sample>
         <TextInput value="valid text" append={<Icon src="/src/check.svg" />} />
         <TextInput placeholder="security number" prepend={<Icon src="/src/alert-outline.svg" />} />
+        <TextInput placeholder="Search" append={<Spinner />} />
       </Sample>
 
       <SectionTitle>Prefix and postfix</SectionTitle>
       <Sample>
         <TextInput placeholder="www.example" prefix="http://" postfix=".com"></TextInput>
+        <TextInput placeholder="Search" postfix={<Icon src="/src/search.svg" />}></TextInput>
       </Sample>
 
       <SectionTitle>Disabled</SectionTitle>
