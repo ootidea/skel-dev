c75f843e3758a393b7add813e28c0c2b989d4809
ootidea <ootidea@gmail.com>
2022-06-18 17:52:13 +0900

    feat: add Popover samples

--- src/page/PopoverComponent.tsx
+++ src/page/PopoverComponent.tsx
@@ -18,8 +18,30 @@ export function PopoverComponent() {
 
       <SectionTitle>Persistent</SectionTitle>
       <Sample>
-        <Popover persistent launcher={({ open }) => <Button onClick={open}>Open</Button>}>
+        <Popover persistent launcher={({ toggle }) => <Button onClick={toggle}>Open</Button>}>
           {({ close }) => <Button onClick={close}>Close</Button>}
+        </Popover>
+      </Sample>
+
+      <SectionTitle>Position</SectionTitle>
+      <Sample>
+        <Popover on="top right" launcher={({ open }) => <Button onClick={open}>Open</Button>}>
+          <div style="padding: 0.5em 1em">Pop up text</div>
+        </Popover>
+      </Sample>
+
+      <SectionTitle>Position in detail</SectionTitle>
+      <Sample>
+        <Popover
+          on="bottom right"
+          joint="bottom right"
+          launcher={({ toggle }) => (
+            <div style="width: 20rem; height: 10rem; background-color: aliceblue">
+              <Button onClick={toggle}>Open in right bottom</Button>
+            </div>
+          )}
+        >
+          <div style="padding: 0.2em 0.4em">Pop up text</div>
         </Popover>
       </Sample>
     </article>
